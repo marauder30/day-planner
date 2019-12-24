02a079edc5220a5778eabf8fc3ea0c5b33047787
Nick Koch <marauder30@gmail.com>
2019-12-23 21:03:29 -0700

    added basic styling and nav

---
+++ script.js
@@ -0,0 +1,6 @@
+//use jquery and bootstrap to construct a simple single day planner
+// planner should use normal business hours, 9 -5
+// each slot represents an hour and has a button to save input (modal?)
+//clicking save will store the input and time in localStorage
+//display current date at top of calendar
+//time slots should be color coded by current/past/future, updating by time 
