2bb6124b318d15eee2348d450aa164827d5494df
Nick Koch <marauder30@gmail.com>
2019-12-25 12:19:47 -0700

    added bootstrap cards to index.html and some psuedocode

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-//use jquery and bootstrap to construct a simple single day planner
+// html references
+
+
+
+
+
+// function a
+
+//function b
+
+//function c
+
+// use jquery and bootstrap to construct a simple single day planner
 // planner should use normal business hours, 9 -5
 // each slot represents an hour and has a button to save input (modal?)
-//clicking save will store the input and time in localStorage
-//display current date at top of calendar
-//time slots should be color coded by current/past/future, updating by time 
+// clicking save will store the input and time in localStorage
+// display current date at top of calendar
+// time slots should be color coded by current/past/future, updating by time 
+
